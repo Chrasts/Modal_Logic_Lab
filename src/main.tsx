4847d47aefcf9b5d2849e5370a229847a9bd6436
Štěpán Chrast <chraststepan@gmail.com>
2026-08-31 13:43:25 +0200

    Load guided mobile presentation styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import { App } from './App'
 import './styles.css'
 import './mobile.css'
 import './mobile-workspace.css'
+import './mobile-guided.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
