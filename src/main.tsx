83deac1d5284132d2fc2e41314268544215f4d24
Štěpán Chrast <chraststepan@gmail.com>
2026-09-21 15:04:23 +0200

    Consolidate mobile CSS patch stack

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,6 @@ import { App } from './App'
 import './styles.css'
 import './relational-design.css'
 import './mobile.css'
-import './mobile-workspace.css'
-import './mobile-guided.css'
-import './mobile-surfaces.css'
-import './mobile-chrome.css'
-import './mobile-stability.css'
-import './mobile-device.css'
-import './mobile-mission-redesign.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
